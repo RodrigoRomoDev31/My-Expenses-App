b16bb74dfa0244a2ef2fbc8ae8024788d0883fb7
Rodrigo Romo Vazquez <rodrigoromoid@gmail.com>
2025-02-24 10:53:44 -0600

    *Message** Chips Colors Fixed

--- feature/home/src/main/java/com/romvaz/feature/home/add/components/ExpenseTypeChipComponent.kt
+++ feature/home/src/main/java/com/romvaz/feature/home/add/components/ExpenseTypeChipComponent.kt
@@ -7,6 +7,7 @@ import androidx.compose.material3.FilterChip
 import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SelectableChipColors
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -31,6 +32,21 @@ fun ExpenseTypeChipComponent(
                     tint = MaterialTheme.myExpensesAppColors.White
                 )
             }
-        }
+        },
+        colors = SelectableChipColors(
+            containerColor = MaterialTheme.colorScheme.surface,
+            labelColor = MaterialTheme.colorScheme.onSurface,
+            leadingIconColor = MaterialTheme.myExpensesAppColors.White,
+            trailingIconColor = MaterialTheme.myExpensesAppColors.White,
+            disabledContainerColor = MaterialTheme.colorScheme.surface,
+            disabledLabelColor = MaterialTheme.colorScheme.onSurface,
+            disabledLeadingIconColor = MaterialTheme.colorScheme.surface,
+            disabledTrailingIconColor = MaterialTheme.colorScheme.surface,
+            selectedContainerColor = MaterialTheme.myExpensesAppColors.Primary100,
+            disabledSelectedContainerColor = MaterialTheme.colorScheme.surface,
+            selectedLabelColor = MaterialTheme.myExpensesAppColors.White,
+            selectedLeadingIconColor = MaterialTheme.myExpensesAppColors.White,
+            selectedTrailingIconColor = MaterialTheme.myExpensesAppColors.White
+        )
     )
 }
